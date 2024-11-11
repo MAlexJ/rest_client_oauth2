a1b6014db7d21ff84c10d35044b2c2a88ca4438d
malexj <alexmaximovj@gmail.com>
2024-11-11 21:45:43 +0200

    refactor build files

---
+++ client_server/src/test/java/com/malex/client_server/ClientServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.malex.client_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
